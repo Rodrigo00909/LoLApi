ad7bc7592efeb4520f1348c3a5f3100553d6eb72
Rodrigo <rodrigotorresmessenzani@hotmail.com>
2021-03-03 21:59:48 -0300

    excepciones

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
 from riotwatcher import LolWatcher
 
-summ = 'Messenzani'
+def Stats(summonerName, sv):
+    summoner = watcher.summoner.by_name(sv, summonerName)
+    print(summoner)
+    stats = watcher.league.by_summoner(sv, summoner['id'])
+    try:
+        tier = stats[0]['tier']
+        rank = stats[0]['rank']
+        lp = stats[0]['leaguePoints']
+        lv = summoner['summonerLevel']
+
+        wins = int(stats[0]['wins'])
+        losses = int(stats[0]['losses'])
+        winrate = int((wins / (wins + losses)) * 100)
+
+        print()
+        print(summonerName,"{",
+            "\n Rank:",tier, rank, "-" ,lp, "pl",
+            "\n Level:", lv, 
+            "\n Winrate:", str(winrate) + "%\n"
+            "}")
+        print()
+    except IndexError:
+        print('El invocador no tiene elo')
+    except OSError as mensaje:
+        print(mensaje)
+
+summ = input('Nombre de invocador? ')
+while summ == "":
+    summ = input('Nombre de invocador? ')
+
+sv = 'la2'
 Apikey = 'tu api'
 watcher = LolWatcher(Apikey)
 
-def Stats(summonerName):
-    summoner = watcher.summoner.by_name('la2', summonerName)
-    stats = watcher.league.by_summoner('la2', summoner['id'])
-
-    tier = stats[0]['tier']
-    rank = stats[0]['rank']
-    lp = stats[0]['leaguePoints']
-
-    wins = int(stats[0]['wins'])
-    losses = int(stats[0]['losses'])
-    winrate = int((wins / (wins + losses)) * 100)
-
-    print()
-    print(summonerName,"{",
-        "\n Rank:",tier, rank, "-" ,lp, "pl"
-        "\n Winrate:", str(winrate) + "%\n"
-        "}")
-    print()
-
-Stats(summ)
+Stats(summ, sv)
